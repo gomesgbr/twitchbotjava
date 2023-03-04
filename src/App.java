9356ff68c26b8a6d69c79a6a27dc58c280a396c8
Gabriel Gomes de Almeida <gabrielg299@gmail.com>
2023-03-04 17:34:57 -0300

    Projeto

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import domain.SocketModel;
+import enums.Command;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        SocketModel socketModel = new SocketModel("irc.chat.twitch.tv", 6667, "x", "x");
+
+        socketModel.connect();
+    }
+}
