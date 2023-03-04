4534d3bac475cc9d844eb53408286d1e42de0efb
Gabriel Gomes de Almeida <gabrielg299@gmail.com>
2023-03-04 17:32:32 -0300

    Objeto account criado

---
+++ src/domain/Account.java
@@ -0,0 +1,24 @@
+ package domain;
+
+public class Account{
+
+    protected String nick;
+    protected String pass;
+
+
+    public String getNick() {
+        return nick;
+    }
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    
+
+ }
